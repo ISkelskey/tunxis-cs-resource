8afa542f073be55303e0d8d618e4f60ecdcf5485
adamwoolford <163425250+adamwoolford@users.noreply.github.com>
2024-03-14 16:03:47 +0000

    HelloWorld in VB

---
+++ visual basic/HelloWorld Solution/HelloWorld Project/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.btnClickMe = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblOutput
+        '
+        Me.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblOutput.Location = New System.Drawing.Point(253, 133)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(100, 23)
+        Me.lblOutput.TabIndex = 0
+        '
+        'btnClickMe
+        '
+        Me.btnClickMe.Location = New System.Drawing.Point(256, 163)
+        Me.btnClickMe.Name = "btnClickMe"
+        Me.btnClickMe.Size = New System.Drawing.Size(75, 23)
+        Me.btnClickMe.TabIndex = 1
+        Me.btnClickMe.Text = "Click Me"
+        Me.btnClickMe.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnClickMe)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lblOutput As Label
+    Friend WithEvents btnClickMe As Button
+End Class
